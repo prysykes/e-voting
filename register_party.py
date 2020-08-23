dd1be6f31a5469890d07235ba4d7cf5f1c4feec4
Emeka Duru <emisykes2000@yahoo.com>
2020-08-23 21:41:33 +0100

    party reg updated

--- register_party.py
+++ register_party.py
@@ -1,2 +1,8 @@
 from party_registration import PartyRegistration
 from party_registration import show_data
+
+print("***Welcome to INEC party registration System*** \n")
+print("***Enter party name press enter to save\n***")
+
+partyone = PartyRegistration()
+show_data()
